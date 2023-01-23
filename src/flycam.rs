68b2759af9c2e45cfaf989816066325c03e469c0
ddorstijn <dannydorstijn@1997@gmail.com>
2023-01-23 16:19:29 +0100

    Fixed camera controls

--- src/flycam.rs
+++ src/flycam.rs
@@ -1,6 +1,5 @@
 use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::prelude::*;
-use bevy::render::camera::Projection;
 
 /// Tags an entity as capable of panning and orbiting.
 #[derive(Component)]
@@ -8,6 +7,7 @@ pub struct PanOrbitCamera {
     /// The "focus point" to orbit around. It is automatically updated when panning the camera
     pub focus: Vec3,
     pub radius: f32,
+    pub speed: f32,
 }
 
 impl Default for PanOrbitCamera {
@@ -15,6 +15,7 @@ impl Default for PanOrbitCamera {
         PanOrbitCamera {
             focus: Vec3::ZERO,
             radius: 5.0,
+            speed: 3.0,
         }
     }
 }
@@ -25,14 +26,19 @@ pub fn pan_orbit_camera(
     mut ev_scroll: EventReader<MouseWheel>,
     input_mouse: Res<Input<MouseButton>>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut PanOrbitCamera, &mut Transform, &Projection)>,
+    mut query: Query<(&mut PanOrbitCamera, &mut Transform)>,
+    time: Res<Time>,
 ) {
+    let (mut pan_orbit, mut transform) = query
+        .get_single_mut()
+        .expect("More than one Player Camera in scene");
+
     // change input mapping for orbit and panning here
     let orbit_button = MouseButton::Right;
 
-    let mut pan = Vec2::ZERO;
     let mut rotation_move = Vec2::ZERO;
     let mut scroll = 0.0;
+    let mut has_moved = false;
 
     // Orbit
     if input_mouse.pressed(orbit_button) {
@@ -41,64 +47,56 @@ pub fn pan_orbit_camera(
         }
     }
 
-    // Pan
-    if keyboard_input.pressed(KeyCode::A) {
-        pan += Vec2::new(5., 0.);
-    }
-    if keyboard_input.pressed(KeyCode::D) {
-        pan += Vec2::new(-5., 0.);
-    }
-    if keyboard_input.pressed(KeyCode::S) {
-        pan += Vec2::new(0.0, -5.0);
-    }
-    if keyboard_input.pressed(KeyCode::W) {
-        pan += Vec2::new(0.0, 5.0);
-    }
-
     // Zoom
     for ev in ev_scroll.iter() {
         scroll += ev.y;
     }
 
-    for (mut pan_orbit, mut transform, projection) in query.iter_mut() {
-        let mut any = false;
-        if rotation_move.length_squared() > 0.0 {
-            any = true;
-            let window = get_primary_window_size(&windows);
-            let delta_x = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
-            let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
-            let yaw = Quat::from_rotation_y(-delta_x);
-            let pitch = Quat::from_rotation_x(-delta_y);
-            transform.rotation = yaw * transform.rotation; // rotate around global y axis
-            transform.rotation = transform.rotation * pitch; // rotate around local x axis
-        } else if pan.length_squared() > 0.0 {
-            any = true;
-            // make panning distance independent of resolution and FOV,
-            let window = get_primary_window_size(&windows);
-            if let Projection::Perspective(projection) = projection {
-                pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
-            }
-            // translate by local axes
-            let right = transform.rotation * Vec3::X * -pan.x;
-            let up = transform.rotation * Vec3::Y * pan.y;
-            // make panning proportional to distance away from focus point
-            let translation = (right + up) * pan_orbit.radius;
-            pan_orbit.focus += translation;
-        } else if scroll.abs() > 0.0 {
-            any = true;
-            pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
-            // dont allow zoom to reach zero or you get stuck
-            pan_orbit.radius = f32::max(pan_orbit.radius, 0.05);
-        }
+    // Pan
+    let forward = Vec3::new(transform.forward().x, 0.0, transform.forward().z).normalize();
+    let left = Vec3::new(transform.left().x, 0.0, transform.left().z).normalize();
+    let speed = pan_orbit.speed;
 
-        if any {
-            // emulating parent/child to make the yaw/y-axis rotation behave like a turntable
-            // parent = x and y rotation
-            // child = z-offset
-            let rot_matrix = Mat3::from_quat(transform.rotation);
-            transform.translation =
-                pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
-        }
+    if keyboard_input.pressed(KeyCode::A) {
+        has_moved = true;
+        pan_orbit.focus += left * time.delta_seconds() * speed;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        has_moved = true;
+        pan_orbit.focus -= left * time.delta_seconds() * speed;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        has_moved = true;
+        pan_orbit.focus -= forward * time.delta_seconds() * speed;
+    }
+    if keyboard_input.pressed(KeyCode::W) {
+        has_moved = true;
+        pan_orbit.focus += forward * time.delta_seconds() * speed;
+    }
+
+    if rotation_move.length_squared() > 0.0 {
+        has_moved = true;
+        let window = get_primary_window_size(&windows);
+        let delta_x = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
+        let delta_y = rotation_move.y / window.y * std::f32::consts::PI;
+        let yaw = Quat::from_rotation_y(-delta_x);
+        let pitch = Quat::from_rotation_x(-delta_y);
+        transform.rotation = yaw * transform.rotation; // rotate around global y axis
+        transform.rotation = transform.rotation * pitch; // rotate around local x axis
+    } else if scroll.abs() > 0.0 {
+        has_moved = true;
+        pan_orbit.radius -= scroll * pan_orbit.radius * 0.2;
+        // dont allow zoom to reach zero or you get stuck
+        pan_orbit.radius = f32::max(pan_orbit.radius, 0.05);
+    }
+
+    if has_moved {
+        // emulating parent/child to make the yaw/y-axis rotation behave like a turntable
+        // parent = x and y rotation
+        // child = z-offset
+        let rot_matrix = Mat3::from_quat(transform.rotation);
+        transform.translation =
+            pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
     }
 }
 
