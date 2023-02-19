178e59065b0ec63714a7999a92550084709f5ba0
ddorstijn <dannydorstijn@1997@gmail.com>
2023-02-19 17:17:52 +0100

    Fixed spline curve implementation

--- src/road/nodegroup.rs
+++ src/road/nodegroup.rs
@@ -34,30 +34,21 @@ pub fn generate_intersection_mesh(
             get_point_on_bezier(sorted_nodes[0].0, sorted_nodes[1].0, 1.0),
         );
 
-        let positions = (0..DETAIL)
-            .flat_map(|i| {
-                let p = get_point_on_bezier(sorted_nodes[0].0, sorted_nodes[1].0, i as f32 / 10.0);
-                println!("({}, {})", p.x, p.z);
-                let offset = sorted_nodes[0].0.left() * (0.5 * sorted_nodes[0].1.lanes as f32);
-
-                vec![p - offset, p + offset]
+        let positions = (0..=DETAIL)
+            .map(|i| {
+                println!("{}", i);
+                get_point_on_bezier(
+                    sorted_nodes[0].0,
+                    sorted_nodes[1].0,
+                    i as f32 / DETAIL as f32,
+                )
             })
             .collect::<Vec<Vec3>>();
 
-        let indices = (0..DETAIL - 1)
-            .flat_map(|i| {
-                let bl = 2 * i;
-                let br = bl + 1;
-                let tr = bl + 3;
-                let tl = bl + 2;
-
-                vec![bl, tl, tr, bl, tr, br]
-            })
-            .collect::<Vec<u32>>();
-
+        let indices = (0..=DETAIL).collect::<Vec<u32>>();
         let normals = vec![Vec3::Y; positions.len()];
 
-        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        let mut mesh = Mesh::new(PrimitiveTopology::LineStrip);
         mesh.set_indices(Some(Indices::U32(indices)));
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
         mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
@@ -71,8 +62,8 @@ fn get_point_on_bezier(t1: &Transform, t2: &Transform, t: f32) -> Vec3 {
     let p0 = t1.translation;
     let p1 = t1.translation + t1.forward() * 2.0;
 
-    let p2 = t2.translation;
-    let p3 = t2.translation + t2.forward() * 4.0;
+    let p2 = t2.translation + t2.forward() * 4.0;
+    let p3 = t2.translation;
 
     let a = p0.lerp(p1, t);
     let b = p1.lerp(p2, t);
