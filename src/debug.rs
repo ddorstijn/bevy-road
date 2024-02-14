d969b3a3d534495288c2cb453617be100340212b
ddorstijn <dannydorstijn@1997@gmail.com>
2024-02-14 13:52:31 +0100

    Rapier raycast and improved build times

--- src/debug.rs
+++ src/debug.rs
@@ -1,142 +1,95 @@
-use std::{f32::consts::PI, sync::Arc};
+use std::f32::consts::PI;
 
 use bevy::{prelude::*, render::render_resource::PrimitiveTopology};
-use bevy_mod_raycast::{immediate::Raycast, CursorRay, DefaultRaycastingPlugin};
-use bevy_prototype_debug_lines::*;
-
-pub struct DebugPlugin;
-impl Plugin for DebugPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(DebugLinesPlugin::default())
-            .add_plugins(DefaultRaycastingPlugin)
-            .add_systems(Startup, test_arc_scene)
-            .add_systems(Update, raycast);
-    }
-}
-
-fn test_arc_scene(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut lines: ResMut<DebugLines>,
-) {
-    let point2 = commands.spawn((
-        RoadSegment {
-            next: None,
-            pbr: PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Box::new(0.25, 0.25, 0.25))),
-                material: materials.add(Color::rgb(1., 1., 1.).into()),
-                transform: Transform::from_translation(Vec3::new(-2.0, 0.0, -2.0)),
-                ..default()
-            },
-        },
-        Name::new("Point 2"),
-    )).id();
-
-    commands.spawn((
-        RoadSegment {
-            next: Some(point2),
-            pbr: PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Box::new(0.25, 0.25, 0.25))),
-                material: materials.add(Color::rgb(1., 1., 1.).into()),
-                transform: Transform::from_translation(Vec3::new(-2.0, 0.0, -2.0)),
-                ..default()
-            },
-        },
-        Name::new("Point 1"),
-    ));
-    
-
-    // Axis
-    lines.line(
-        Vec3::new(-0.1, 0., 0.),
-        Vec3::new(0.1, 0., 0.),
-        f32::INFINITY,
-    );
-    lines.line(
-        Vec3::new(0., -0.1, 0.),
-        Vec3::new(0., 0.1, 0.),
-        f32::INFINITY,
-    );
-    lines.line(
-        Vec3::new(0., 0., -0.1),
-        Vec3::new(0., 0., 0.1),
-        f32::INFINITY,
-    );
-}
 
 #[derive(Component)]
-struct RoadSegment {
-    pbr: PbrBundle, 
+pub struct RoadEdge {
+    center: Vec3,
+    start_angle: f32,
+    end_angle: f32,
+    radius: f32,
+    length: f32,
+    clockwise: bool,
 }
 
-impl RoadSegment {
-    fn generate_arc_points(&self, center: Vec3, endpoint: &RoadSegment) -> Vec<Vec3> {
-        let start = self.pbr.transform.translation;
-        let end = endpoint.pbr.transform.translation;
-        
-        let radius = (start - center).length();
-        let start_angle = (start.z - center.z).atan2(start.x - center.x);
-        let end_angle = (end.z - center.z).atan2(end.x - center.x);
+impl RoadEdge {
+    pub fn new(start: &GlobalTransform, endpoint: Vec3) -> Self {
+        let startpoint = start.translation();
+        let midpoint: Vec3 = (endpoint + startpoint) / 2.0;
+        let bisector: Vec3 = (endpoint - startpoint).any_orthogonal_vector().normalize();
 
-        let clockwise = self.pbr.global_transform.forward().angle_between(end - start).is_sign_negative();
-        let (start_angle, end_angle) = match clockwise {
-            true if start_angle < end_angle => (start_angle + 2.0 * PI, end_angle),
-            false if end_angle < start_angle => (start_angle, end_angle + 2.0 * PI),
-            _ => (start_angle, end_angle),
-        };
-    
-        let arc_length = radius * (end_angle - start_angle).abs();
-    
-        // Calculate the number of steps, ensuring there is approximately 1 unit arc length between points
-        let num_steps = arc_length.ceil() as usize;
-        // Adjust the step size based on the total arc length and number of steps
-        let step = (end_angle - start_angle).abs() / num_steps as f32;
-    
-        (0..=num_steps)
-            .map(|i| {
-                let inc = step * i as f32;
-                let angle = start_angle + inc;
-                let x = center.x + radius * angle.cos();
-                let y = start.y;
-                let z = center.z + radius * angle.sin();
-                Vec3::new(x, y, z)
-            })
-            .collect()
-    }
-
-    fn generate_line_points(&self) -> Vec<Vec3> {
-        unimplemented!()
-    }
-
-    fn get_center(&self, endpoint: &RoadSegment) -> Option<Vec3> {
-        let start: Vec3 = self.pbr.transform.translation;
-        let start_direction: Vec3 = self.pbr.global_transform.right();
-        let end = endpoint.pbr.transform.translation;
-        let midpoint: Vec3 = (end + start) / 2.0;
-        let bisector: Vec3 = (end - start).any_orthogonal_vector().normalize();;
-
-        let direction = midpoint - start;
-        let cross1 = start_direction.cross(bisector);
+        let direction = midpoint - startpoint;
+        let cross1 = start.right().cross(bisector);
         let cross2 = direction.cross(bisector);
     
         let planar_factor = direction.dot(cross1).abs();
     
         //is coplanar, and not parrallel
-        if planar_factor < 0.0001 && cross1.length_squared() > 0.0001 {
-            let s = cross2.dot(cross1) / cross1.length_squared();
-            Some(start + (start_direction * s))
-        } else {
-            None
+        let (center, radius) = match planar_factor < 0.0001 && cross1.length_squared() > 0.0001 {
+            true => {
+                let s = cross2.dot(cross1) / cross1.length_squared();
+                let center = startpoint + start.right() * s;
+                (center, (startpoint - center).length())
+            },
+            false => (midpoint,  0.0)
+        };
+
+        let clockwise = start.forward().angle_between(endpoint - startpoint).is_sign_negative();
+        let start_angle = (startpoint.z - center.z).atan2(startpoint.x - center.x);
+        let end_angle = (endpoint.z - center.z).atan2(endpoint.x - center.x);
+        let (start_angle, end_angle) = match clockwise {
+            true if start_angle < end_angle => (start_angle + 2.0 * PI, end_angle),
+            false if end_angle < start_angle => (start_angle, end_angle + 2.0 * PI),
+            _ => (start_angle, end_angle),
+        };
+
+        Self {
+            center,
+            start_angle,
+            end_angle,
+            radius,
+            clockwise,
+            length: radius * (end_angle - start_angle).abs(),
         }
     }
 
-    fn generate_mesh(&self, end: &RoadSegment) -> Mesh {
-        let points = match self.get_center(end) {
-            Some(center) => self.generate_arc_points(center, end),
-            None => self.generate_line_points()
-        };
+    fn interpolate_arc(&self, length: f32) -> Transform {   
+        let inc = length / self.radius * (f32::from(self.clockwise) * 2. - 1.);
+        let angle = self.start_angle + inc;
+        let x = self.center.x + self.radius * angle.cos();
+        let y = self.center.y;
+        let z = self.center.z + self.radius * angle.sin();
 
+        let position = Vec3::new(x, y, z);
+        let radius_vector = position - self.center;
+        let up_vector = Vec3::new(0.0, 1.0, 0.0);
+        let forward_vector = up_vector.cross(radius_vector).normalize();
+        let rotation = Quat::from_rotation_arc(Vec3::new(0.0, 0.0, -1.0), forward_vector);
+
+        let mut transform = Transform::default();
+        transform.translation = position;
+        transform.rotation = rotation;
+        transform
+    }
+
+    fn interpolate_line(&self, length: f32) -> Transform {
+        unimplemented!()
+    }
+
+    pub fn interpolate(&self, length: f32) -> Transform {
+        match self.radius < 0.005 {
+            true => self.interpolate_line(length),
+            false =>  self.interpolate_arc(length)
+        }
+    }
+
+    pub fn get_end_transform(&self) -> Transform {
+        self.interpolate(self.length)
+    }
+    
+    pub fn generate_mesh(&self) -> Mesh {    
+        let points = (0..=self.length.ceil() as usize).map(|i| self.interpolate_arc(i as f32).translation).collect::<Vec<Vec3>>();
+        
         let detail = points.len();
         let normals = vec![Vec3::Y; detail + 1];
         let indices = (0..=detail as u32).collect::<Vec<u32>>();
@@ -147,18 +100,5 @@ impl RoadSegment {
         mesh.set_indices(Some(bevy::render::mesh::Indices::U32(indices)));
 
         mesh
-    }
-}
-
-fn raycast(mut query: Query<(&RoadSegment, &mut Handle<Mesh>)>, query2: Query<&RoadSegment>, mut meshes: ResMut<Assets<Mesh>>, cursor_ray: Res<CursorRay>, mut raycast: Raycast) {
-    if let Some(cursor_ray) = **cursor_ray {
-        if let Some((_, hit)) = raycast.cast_ray(cursor_ray, &default()).first() {
-            for (road, mut handle) in &mut query {
-                if let Some(end) = road.next {
-                    let component = query2.get_component::<RoadSegment>(end).unwrap();
-                    *handle = meshes.add(road.generate_mesh(component));
-                }
-            } 
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use bevy::{pbr::wireframe::WireframePlugin, prelude::*};
+use bevy::{
+    input::common_conditions::input_just_released, pbr::wireframe::WireframePlugin, prelude::*,
+    window::PrimaryWindow,
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_mod_picking::prelude::*;
+use bevy_rapier3d::prelude::*;
+use camera::PanOrbitCamera;
+use debug::RoadEdge;
 
 // use debug::DebugPlugin;
-//use road::RoadPlugin;
+// use road::RoadPlugin;
 
 // pub mod road;
-// mod debug;
+mod camera;
+mod debug;
 
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(low_latency_window_plugin()),
-            DefaultPickingPlugins,
+            DefaultPlugins,
+            RapierPhysicsPlugin::<NoUserData>::default(),
             WireframePlugin,
             WorldInspectorPlugin::default(),
-            GamePlugin
+            GamePlugin,
         ))
         .run();
 }
@@ -27,8 +33,21 @@ impl Plugin for GamePlugin {
         app
             // .add_plugins(RoadPlugin)
             // .add_plugins(DebugPlugin)
-
-            .add_systems(Startup, setup_scene);
+            .add_plugins(camera::CameraPlugin)
+            .register_type::<SelectedEntity>()
+            .init_resource::<SelectedEntity>()
+            .add_systems(Startup, setup_scene)
+            .add_systems(
+                PreUpdate,
+                handle_selection.run_if(input_just_released(MouseButton::Left)),
+            )
+            .add_systems(
+                Update,
+                (
+                    create_road_placeholder.run_if(not(any_with_component::<RoadPlaceholder>())),
+                    move_road_placeholder.run_if(any_with_component::<RoadPlaceholder>()),
+                ),
+            ); //.add_systems(Update, create_road_segment);
     }
 }
 
@@ -40,10 +59,12 @@ fn setup_scene(
     // Environment and player
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(0.0, 5., 0.0)).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_translation(Vec3::new(0.0, 5., 0.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         },
-        Name::new("Player")
+        Name::new("Player"),
+        PanOrbitCamera { ..default() },
     ));
 
     commands.spawn((
@@ -59,27 +80,155 @@ fn setup_scene(
             },
             ..default()
         },
-        Name::new("Sun")
+        Name::new("Sun"),
     ));
 
     commands.spawn((
         PbrBundle {
             material: materials.add(Color::rgb(0.0, 0.4, 0.4).into()),
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 20., ..default() })),
+            mesh: meshes.add(Mesh::from(shape::Plane {
+                size: 20.,
+                ..default()
+            })),
             transform: Transform::from_xyz(0.0, -1.0, 0.0),
             ..default()
         },
-        // PickableBundle::default(), // Optional: adds selection, highlighting, and helper components.
-        On::<Pointer<Click>>::target_commands_mut(|click, _target_commands| {
-            println!("{:?}", click);
-        }),
-        Name::new("Ground")
+        Collider::cuboid(10.0, 0.1, 10.0),
+        CollisionGroups::new(Group::GROUP_1, Group::GROUP_1),
+        Name::new("Ground"),
+    ));
+
+    commands.spawn((
+        PbrBundle {
+            material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+            mesh: meshes.add(Mesh::from(shape::Cube {
+                size: 0.5,
+                ..default()
+            })),
+            ..default()
+        },
+        Collider::cuboid(0.5, 0.5, 0.5),
+        CollisionGroups::new(Group::GROUP_2, Group::GROUP_1),
+        RoadSpawner,
     ));
 }
 
-pub struct RaycastPlugin;
-impl Plugin for RaycastPlugin {
-    fn build(&self, app: &mut App) {
-        // app.add_systems(Update, raycast);
-    }
+#[derive(Reflect, Resource, Default)]
+#[reflect(Resource)]
+struct SelectedEntity(Option<Entity>);
+
+fn handle_selection(
+    rapier_context: Res<RapierContext>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<PanOrbitCamera>>,
+    mut selection: ResMut<SelectedEntity>,
+) {
+    let window = window_query.single();
+    let (camera, camera_transform) = camera_query.single();
+    let Some(ray) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+    else {
+        return;
+    };
+
+    let max_toi = 100.0;
+    let solid = true;
+    let filter = QueryFilter::new().groups(CollisionGroups::new(
+        Group::all().difference(Group::GROUP_2),
+        Group::all().difference(Group::GROUP_1),
+    ));
+
+    let Some((entity, _)) =
+        rapier_context.cast_ray(ray.origin, ray.direction, max_toi, solid, filter)
+    else {
+        selection.0 = None;
+        return;
+    };
+
+    selection.0 = Some(entity);
 }
+
+#[derive(Component)]
+struct RoadSpawner;
+
+#[derive(Component)]
+struct RoadNode;
+
+#[derive(Component)]
+struct RoadPlaceholder {
+    start_position: GlobalTransform,
+}
+
+fn create_road_placeholder(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<&GlobalTransform, With<RoadSpawner>>,
+    selected_entity: Res<SelectedEntity>,
+) {
+    let Some(entity) = selected_entity.0 else {
+        return;
+    };
+    let Ok(selected_transform) = query.get(entity) else {
+        return;
+    };
+
+    commands.spawn((
+        PbrBundle {
+            transform: selected_transform.compute_transform(),
+            material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
+            mesh: meshes.add(Mesh::from(shape::Cube {
+                size: 0.5,
+                ..default()
+            })),
+            ..default()
+        },
+        RoadPlaceholder {
+            start_position: selected_transform.clone(),
+        },
+    ));
+}
+
+fn move_road_placeholder(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<PanOrbitCamera>>,
+    mut query: Query<(Entity, &RoadPlaceholder, &mut Transform)>,
+) {
+    let Ok((entity, placeholder, mut transform)) = query.get_single_mut() else {
+        return;
+    };
+
+    let window = window_query.single();
+    let (camera, camera_transform) = camera_query.single();
+    let Some(ray) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+    else {
+        return;
+    };
+
+    let max_toi = 100.0;
+    let solid = true;
+    let filter =
+        QueryFilter::default().groups(CollisionGroups::new(Group::GROUP_1, Group::GROUP_1));
+    let Some((_, toi)) = rapier_context.cast_ray(ray.origin, ray.direction, max_toi, solid, filter)
+    else {
+        return;
+    };
+
+    transform.translation = ray.origin + ray.direction * toi;
+    commands.entity(entity).insert(RoadEdge::new(&placeholder.start_position, transform.translation));
+}
+
+// fn create_road_segment(query: Query<&GlobalTransform, Or<(With<RoadSpawner>, With<RoadNode>)>>) {
+// if let Ok(road_start) = query.get() {
+// road_start
+
+// let edge = RoadEdge::new(road_start, hover.read().last().unwrap().position.unwrap());
+// let mesh = edge.generate_mesh();
+// println!("{:?}", mesh);
+// }
+// }
