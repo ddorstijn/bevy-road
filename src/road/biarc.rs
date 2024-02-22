20a8e0fa380b1999182818af7237e76839cc4d15
ddorstijn <dannydorstijn@1997@gmail.com>
2024-02-22 23:14:13 +0100

    Some cases with biarc working and tested. Rotation seems to not be handled well

--- src/road/biarc.rs
+++ src/road/biarc.rs
@@ -6,45 +6,41 @@ pub fn compute_biarc(
     start: GlobalTransform,
     end: GlobalTransform,
     lanes: u8,
-) -> (RoadEdge, RoadEdge) {
+) -> (RoadEdge, Transform, RoadEdge) {
     let midpoint = compute_midpoint(start, end);
+    println!("{:?}", midpoint);
 
-    let start_local_mid = start
-        .compute_matrix()
-        .inverse()
-        .transform_point(midpoint.translation);
+    let start_local_mid = start.compute_matrix().inverse().transform_point(midpoint);
 
     let edge1 = RoadEdge::new(start_local_mid, lanes);
 
-    let mid_local_end = midpoint
+    let mid_transform = edge1.get_end_transform(None);
+
+    let mid_local_end = mid_transform
         .compute_matrix()
         .inverse()
         .transform_point(end.translation());
 
     let edge2 = RoadEdge::new(mid_local_end, lanes);
-    (edge1, edge2)
+    (edge1, mid_transform, edge2)
 }
 
-fn compute_midpoint(start: GlobalTransform, end: GlobalTransform) -> Transform {
+fn compute_midpoint(start: GlobalTransform, end: GlobalTransform) -> Vec3 {
     let startpoint = start.translation().xz();
     let endpoint = end.translation().xz();
     let start_tangent = start.forward().xz();
     let end_tangent = end.forward().xz();
 
     let v = endpoint - startpoint;
+    let v_dot_t = v.dot(start_tangent + end_tangent);
 
     // compute the denominator for the quadratic formula
-    let t = start_tangent + end_tangent;
-
-    let v_dot_t = v.dot(t);
-    // abc-formula
     let denominator = 2.0 * (1.0 - start_tangent.dot(end_tangent));
 
     // if the special case d is infinity, the only solution is to
     // interpolate across two semicircles
     if denominator < 0.001 && v.dot(end_tangent).abs() < 0.001 {
-        return Transform::from_translation(v.extend(0.0).xzy() * 0.5)
-            .looking_to(v.perp().normalize().extend(0.0).xzy(), Vec3::Y);
+        return v.extend(0.0).xzy() * 0.5;
     }
 
     // if the quadratic formula denominator is zero, the tangents are equal
@@ -61,8 +57,5 @@ fn compute_midpoint(start: GlobalTransform, end: GlobalTransform) -> Transform {
     // compute the connection point (i.e. the mid point)
     let midpoint = (endpoint + startpoint + (start_tangent - end_tangent) * d) * 0.5;
 
-    let q2 = endpoint - d * end_tangent;
-
-    Transform::from_translation(midpoint.extend(0.0).xzy())
-        .looking_at(q2.extend(0.0).xzy(), Vec3::Y)
+    midpoint.extend(0.0).xzy()
 }
--- src/road/mod.rs
+++ src/road/mod.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use petgraph::graph::Graph;
 
+use crate::raycast::RaycastGroup;
+
 use self::edge::RoadEdge;
 
 pub mod biarc;
@@ -16,6 +18,44 @@ impl Plugin for RoadPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<RoadGraph>()
             .register_type::<RoadEdge>()
+            .add_systems(Startup, test_biarc)
             .add_plugins(placeholder::PlaceholderPlugin);
     }
+}
+
+// We can create our own gizmo config group!
+#[derive(Default, Reflect, GizmoConfigGroup)]
+pub struct BiarcGizmos {}
+
+fn test_biarc(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let start = GlobalTransform::from(Transform::IDENTITY);
+    let end = GlobalTransform::from(Transform {
+        translation: Vec3::new(10.0, 0.0, -10.0),
+        rotation: Quat::from_axis_angle(Vec3::Y, std::f32::consts::PI),
+        ..default()
+    });
+
+    let (edge1, midpoint, edge2) = biarc::compute_biarc(start, end, 1);
+
+    commands.spawn((
+        Name::new("RoadPlaceholder"),
+        PbrBundle {
+            transform: start.compute_transform(),
+            mesh: meshes.add(edge1.mesh()),
+            ..default()
+        },
+        edge1,
+        RaycastGroup { group: 0b1 },
+    ));
+
+    commands.spawn((
+        Name::new("RoadPlaceholder"),
+        PbrBundle {
+            transform: midpoint,
+            mesh: meshes.add(edge2.mesh()),
+            ..default()
+        },
+        edge2,
+        RaycastGroup { group: 0b1 },
+    ));
 }
